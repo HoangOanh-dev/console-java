c0d62a8ba3a02ae7637104566eb232e9a7e6e4be
HoangOanh-dev <oanht3058@gmail.com>
2025-08-03 09:14:13 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        // Tạo đối tượng Scanner để nhập dữ liệu từ bàn phím
+        Scanner scanner = new Scanner(System.in);
+
+        // Nhập tên
+        System.out.print("Nhập tên của bạn: ");
+        String name = scanner.nextLine();
+
+        // In ra lời chào
+        System.out.println("Xin chào, " + name + "! Chúc bạn một ngày tốt lành.");
+
+        // Đóng Scanner
+        scanner.close();
+    }
+}
